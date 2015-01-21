ba1810b12721def0846a30f7f347d7fe89090c48
Jacob Ryan <ryanjm@rose-hulman.edu>
2015-01-21 17:06:48 -0500

    Copied over existing project documents including diagrams and SQL scripts

---
+++ Drop Tables.sql
@@ -0,0 +1,10 @@
+USE [333-2014-Driftr]
+GO
+
+DROP TABLE [Preferences]
+DROP TABLE [EventParticipant]
+DROP TABLE [Event]
+DROP TABLE [Location]
+DROP TABLE [Friend]
+DROP TABLE [Vehicle]
+DROP TABLE [User]
