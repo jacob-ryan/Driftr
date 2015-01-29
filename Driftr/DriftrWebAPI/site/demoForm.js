f8f63124333bffc9eaef10e37a0c590483888399
davisnygren <davisnygren@gmail.com>
2015-01-29 14:34:53 -0500

    created demo website

---
+++ Driftr/DriftrWebAPI/site/demoForm.js
@@ -0,0 +1,16 @@
+﻿window.onload = function () {
+    document.getElementById("submitButton").onclick = submitClick();
+
+
+
+};
+
+function submitClick() {
+    $.post({
+        url: "/api/User",
+        data: {
+            email: "xyz",
+            name: "blah"
+        }
+    });
+};
