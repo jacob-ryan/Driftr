85cc9bc27d5f9753da230ccfb16201d809d3a366
Jacob Ryan <ryanjm@rose-hulman.edu>
2015-02-08 18:50:10 -0500

    Added Event API

---
+++ Driftr/DriftrWebAPI/Controllers/EventController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Security;
+using DriftrWebAPI.Helpers;
+using DriftrWebAPI.Models;
+using DriftrWebAPI.Sprocs;
+
+namespace DriftrWebAPI.Controllers
+{
+	public class EventController : BaseController
+	{
+		// GET: api/Event
+		[Authorize]
+		public HttpResponseMessage Get()
+		{
+			SqlDataReader reader = SprocEvent.getAll(this.connection);
+			List<Event> events = new List<Event>();
+			while (reader.Read())
+			{
+				Event e = new Event();
+				e.id = (int) reader["id"];
+				e.userEmail = reader["userEmail"].ToString();
+				e.locationId = (int) reader["locationId"];
+				e.date = (DateTime) reader["eventDate"];
+				e.theme = reader["theme"].ToString();
+				e.description = reader["description"].ToString();
+				e.wasBusted = (bool) reader["wasBusted"];
+
+				events.Add(e);
+			}
+			return Request.CreateResponse(HttpStatusCode.OK, events);
+		}
+
+		// GET: api/Event/<id>
+		[Authorize]
+		public HttpResponseMessage Get(int id)
+		{
+			SqlDataReader reader = SprocEvent.get(this.connection, id);
+			reader.Read();
+
+			Event e = new Event();
+			e.id = (int) reader["id"];
+			e.userEmail = reader["userEmail"].ToString();
+			e.locationId = (int) reader["locationId"];
+			e.date = (DateTime) reader["eventDate"];
+			e.theme = reader["theme"].ToString();
+			e.description = reader["description"].ToString();
+			e.wasBusted = (bool) reader["wasBusted"];
+
+			return Request.CreateResponse(HttpStatusCode.OK, e);
+		}
+
+		// POST: api/Event
+		public HttpResponseMessage Post(Event e)
+		{
+			SprocEvent.add(this.connection, e.userEmail, e.locationId, e.date, e.theme, e.description, e.wasBusted);
+			return Request.CreateResponse(HttpStatusCode.Created, true);
+		}
+
+		// PUT: api/Event/<id>
+		public HttpResponseMessage Put(int id, Event e)
+		{
+			SprocEvent.update(this.connection, id, e.userEmail, e.locationId, e.date, e.theme, e.description, e.wasBusted);
+			return Request.CreateResponse(HttpStatusCode.Created, true);
+		}
+	}
+}
---
+++ Driftr/DriftrWebAPI/Sprocs/SprocEvent.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace DriftrWebAPI.Sprocs
+{
+	public class SprocEvent
+	{
+		public static SqlDataReader getAll(SqlConnection connection)
+		{
+			SqlCommand command = new SqlCommand("EXEC [select_all_events];", connection);
+
+			return command.ExecuteReader();
+		}
+
+		public static SqlDataReader get(SqlConnection connection, int id)
+		{
+			SqlCommand command = new SqlCommand("EXEC [select_event] @Id;", connection);
+			command.Parameters.Add(new SqlParameter("@Id", id));
+
+			return command.ExecuteReader();
+		}
+
+		public static void add(SqlConnection connection, string userEmail, int locationId, DateTime eventDate, string theme, string description, bool wasBusted)
+		{
+			SqlCommand command = new SqlCommand("EXEC [insert_event] @UserEmail, @LocationId, @EventDate, @Theme, @Description, @WasBusted;", connection);
+			command.Parameters.Add(new SqlParameter("@UserEmail", userEmail));
+			command.Parameters.Add(new SqlParameter("@LocationId", locationId));
+			command.Parameters.Add(new SqlParameter("@EventDate", eventDate));
+			command.Parameters.Add(new SqlParameter("@Theme", theme));
+			command.Parameters.Add(new SqlParameter("@Description", description));
+			command.Parameters.Add(new SqlParameter("@WasBusted", wasBusted));
+
+			command.ExecuteNonQuery();
+		}
+
+		public static void update(SqlConnection connection, int id, string userEmail, int locationId, DateTime eventDate, string theme, string description, bool wasBusted)
+		{
+			SqlCommand command = new SqlCommand("EXEC [update_event] @Id, @UserEmail, @LocationId, @EventDate, @Theme, @Description, @WasBusted;", connection);
+			command.Parameters.Add(new SqlParameter("@Id", id));
+			command.Parameters.Add(new SqlParameter("@UserEmail", userEmail));
+			command.Parameters.Add(new SqlParameter("@LocationId", locationId));
+			command.Parameters.Add(new SqlParameter("@EventDate", eventDate));
+			command.Parameters.Add(new SqlParameter("@Theme", theme));
+			command.Parameters.Add(new SqlParameter("@Description", description));
+			command.Parameters.Add(new SqlParameter("@WasBusted", wasBusted));
+
+			command.ExecuteNonQuery();
+		}
+	}
+}
