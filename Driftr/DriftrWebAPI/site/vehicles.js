353b3a354744d94d28d807e99ef554a2351eaaca
davisnygren <davisnygren@gmail.com>
2015-02-08 18:06:07 -0500

    Added vehicles UI page

---
+++ Driftr/DriftrWebAPI/site/vehicles.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    Driftr.api("GET", "Vehicle", null).done(function (vehicles) {
+        //console.log(vehicles);
+    });
+
+    $("form").on("submit", function (e) {
+        e.preventDefault();
+
+        Driftr.api("GET", "Login", null).done(function (curUser) {
+            var email = curUser.email;
+
+            console.log(email);
+            var data = {
+                userEmail: email,
+                active: 1,
+                make: $("#addcar-make").val(),
+                model: $("#addcar-model").val(),
+                year: $("#addcar-year").val(),
+                color: $("#addcar-color").val(),
+                description: $("#addcar-description").val()
+            };
+
+
+            Driftr.api("POST", "Vehicle", data).done(function () {
+                window.location = "dashboard.html";
+            });
+        });
+
+    });
+});
