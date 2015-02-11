93e5aa76c0109a138f3b801874e25a0f9341f4ed
davisnygren <davisnygren@gmail.com>
2015-02-11 16:29:30 -0500

    Events page partially implemented
    Loads events but doesn't add them correctly

---
+++ Driftr/DriftrWebAPI/site/events.js
@@ -0,0 +1,40 @@
+﻿/* JavaScript file for events.html */
+
+$(document).ready(function () {
+    Driftr.api("GET", "Login", null).done(function (curUser) {
+
+        Driftr.api("GET", "Event", null).done(function (events) {
+            console.log(events);
+            for (var i = 0; i < events.length; i++) {
+                var event = events[i].locationId + " / " + events[i].theme + " / "
+                    + new Date(events[i].date).toDateString();
+
+                var li = document.createElement("li");
+                li.innerHTML = event;
+                document.getElementById("eventlist").appendChild(li);
+            }
+        });
+    });
+
+
+    $("form").on("submit", function (e) {
+        e.preventDefault();
+
+        Driftr.api("GET", "Login", null).done(function (curUser) {
+            console.log(curUser.email);
+
+            var data = {
+                userEmail: curUser.email,
+                locationId: parseInt($("#addevent-location").val()),
+                date: new Date($("#addevent-date").val()),
+                theme: $("#addevent-theme").val(),
+                description: $("#addevent-description").val()
+            };
+
+            Driftr.api("POST", "Event", data).done(function () {
+                window.location = "dashboard.html";
+            });
+        });
+
+    });
+});
