c9bc02778cbc076f7d860538a0c3d3c3dc66895e
davisnygren <davisnygren@gmail.com>
2015-02-18 14:56:00 -0500

    Events: added table format*
    *still need to enable join buttons

--- Driftr/DriftrWebAPI/site/events.js
+++ Driftr/DriftrWebAPI/site/events.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function()
 {
-	$("form").validate({
+	/*$("form").validate({
 		errorClass: 'help-block animation-slideDown', // You can change the animation class for a different entrance animation - check animations page
 		errorElement: 'div',
 		errorPlacement: function(error, e)
@@ -31,16 +31,20 @@
 				date: true
 			}
 		}
-	});
+	});*/
 
     Driftr.api("GET", "Login", null).done(function (curUser) {
 
         Driftr.api("GET", "Event", null).done(function (events) {
             console.log(events);
+            populateEvents(events);
+            /*
             for (var i = 0; i < events.length; i++) {
                 var event = events[i].locationId + " / " + events[i].theme + " / "
                     + new Date(events[i].date).toDateString() + "  ";
                 console.log(event);
+
+                //getLocation(events[i]);
 
                 var li = document.createElement("li");
                 li.innerHTML = event;
@@ -53,25 +57,11 @@
 
                 button.onclick = scopepreserver(events[i].id, curUser.email);
 
-            }
+            }*/
         });
     });
 
-    //http://www.howtocreate.co.uk/referencedvariables.html
-    function scopepreserver(id,email) {
-        return function () {
-            console.log(id);
 
-            var data = {
-                userEmail: email,
-                eventID: id,
-                placement: 0
-            };
-            Driftr.api("POST", "EventParticipant", data).done(function () {
-                window.location = "dashboard.html";
-            });
-        }
-    }
 
     setupJoinButton = function (event, email) {
         console.log(event.id);
@@ -108,3 +98,105 @@
 
     };
 });
+
+//gets the location from the event's location id and adds it 
+//  to the innerHTML of the location cell in the table.
+var getLocation = function (event, bodyLocation) {
+    Driftr.api("GET", "Location/" + event.locationId, null).done(function (location) {
+        bodyLocation.innerHTML = location.address;
+    });
+};
+
+//http://www.howtocreate.co.uk/referencedvariables.html
+function scopepreserver(id, email) {
+    return function () {
+        console.log(id);
+
+        var data = {
+            userEmail: email,
+            eventID: id,
+            placement: 0
+        };
+        Driftr.api("POST", "EventParticipant", data).done(function () {
+            window.location = "dashboard.html";
+        });
+    }
+}
+
+var populateEvents = function (events) {
+    //Create the base for the table
+    var table = document.createElement("table");
+    table.className = "table";
+
+    //build the headdings for the table
+    var thead = document.createElement("thead");
+    var headTr = document.createElement("tr");
+    var headTheme = document.createElement("th");
+    var headDate = document.createElement("th");
+    var headLocation = document.createElement("th");
+    var headDescription = document.createElement("th");
+    var headDelete = document.createElement("th");
+
+    //set text of each head
+    headTheme.innerHTML = "Theme";
+    headDate.innerHTML = "Date";
+    headLocation.innerHTML = "Location";
+    headDescription.innerHTML = "Description";
+
+    headDelete.innerHTML = "";
+    //append headding stuff together
+    headTr.appendChild(headTheme);
+    headTr.appendChild(headDate);
+    headTr.appendChild(headLocation);
+    headTr.appendChild(headDescription);
+    headTr.appendChild(headDelete);
+    thead.appendChild(headTr);
+    //append thead into table
+    table.appendChild(thead);
+
+    //create tbody to add rows to
+    var tbody = document.createElement("tbody");
+
+    for (var i = 0; i < events.length; i++) {
+        //build row and its columns
+        var bodyTr = document.createElement("tr");
+        var bodyTheme = document.createElement("td");
+        var bodyDate = document.createElement("td");
+        var bodyLocation = document.createElement("td");
+        var bodyDescription = document.createElement("td");
+        var bodyDelete = document.createElement("td");
+
+        //********SPECIAL FOR DELETE BUTTON*********
+        //create button
+        var buttonDelete = document.createElement("button");
+        buttonDelete.setAttribute("type", "button");
+        buttonDelete.className = "btn btn-sm btn-success";
+        buttonDelete.innerHTML = "JOIN"
+        buttonDelete.setAttribute("value", events[i]['id']);
+        //set onClick action for delete button
+        //buttonDelete.onclick = scopepreserver(events[i].id, curUser.email);
+        //******************************************
+
+        //populate the columns
+        bodyTheme.innerHTML = events[i]['theme'];
+        bodyDate.innerHTML = new Date(events[i]['date']).toDateString();
+        var curEvent = events[i];
+        getLocation(curEvent, bodyLocation); //add location address
+        bodyDescription.innerHTML = events[i]['description'];
+        bodyDelete.appendChild(buttonDelete);
+
+        //append all columns into row
+        bodyTr.appendChild(bodyTheme);
+        bodyTr.appendChild(bodyDate);
+        bodyTr.appendChild(bodyLocation);
+        bodyTr.appendChild(bodyDescription);
+        bodyTr.appendChild(bodyDelete);
+
+        //append row into body of table
+        tbody.appendChild(bodyTr);
+    }
+    //append tbody into table
+    table.appendChild(tbody);
+    //append table into doc
+    $("#eventlist").append(table);
+};
