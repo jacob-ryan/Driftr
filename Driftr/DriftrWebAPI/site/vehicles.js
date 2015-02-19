6ea8cef1121d87a6cebfa5630a843db1cde47e7b
Jacob Ryan <ryanjm@rose-hulman.edu>
2015-02-18 23:15:10 -0500

    Implemented editing a vehicle

--- Driftr/DriftrWebAPI/site/vehicles.js
+++ Driftr/DriftrWebAPI/site/vehicles.js
@@ -39,133 +39,97 @@
 		}
 	});
 
-    Driftr.api("GET", "Login", null).done(function (curUser) {
+	Driftr.api("GET", "Login", null).done(function(user)
+	{
+		Driftr.api("GET", "Vehicle?email=" + user.email, null).done(function(cars)
+		{
+			for (var i = 0; i < cars.length; i += 1)
+			{
+				var car = cars[i];
+				var html = "<tr>";
+				html += "<td>" + car.make + "</td>";
+				html += "<td>" + car.model + "</td>";
+				html += "<td>" + car.color + "</td>";
+				html += "<td>" + car.year + "</td>";
+				html += "<td>" + car.description + "</td>";
+				html += "<td>" + (car.active ? "Yes" : "No") + "</td>";
+				html += "<td><button class='btn btn-sm btn-primary' onclick='editCar(" + car.id + ");'>Edit</button></td>"
+				html += "</tr>";
+				$(".table").append(html);
+			}
+		});
+	});
 
-        Driftr.api("GET", "Login", null).done(function (curUser) {
-            var email = curUser.email;
-            Driftr.api("GET", "Vehicle?email=" + email, null).done(function (vehicles) {
-                populateVehicles(vehicles);
-            });
-        });
-    });
+	window.editCar = function(id)
+	{
+		window.editId = id;
 
+		Driftr.api("GET", "Login", null).done(function(user)
+		{
+			Driftr.api("GET", "Vehicle?email=" + user.email, null).done(function(cars)
+			{
+				for (var i = 0; i < cars.length; i += 1)
+				{
+					var car = cars[i];
+					if (car.id == id)
+					{
+						$("#car-edit-make").val(car.make);
+						$("#car-edit-model").val(car.model);
+						$("#car-edit-year").val(car.year);
+						$("#car-edit-color").val(car.color);
+						$("#car-edit-description").val(car.description);
+						if (car.active)
+						{
+							$("#car-edit-active").attr("checked", "checked");
+						}
+						$("#edit-container").fadeIn();
 
-    window.submitForm = function()
-    {
-        Driftr.api("GET", "Login", null).done(function (curUser) {
-            var email = curUser.email;
+						break;
+					}
+				}
+			});
+		});
+	};
 
-            console.log(email);
+	window.submitAdd = function()
+	{
+		Driftr.api("GET", "Login", null).done(function(user)
+		{
+			var data = {
+				userEmail: user.email,
+				active: true,
+				make: $("#car-add-make").val(),
+				model: $("#car-add-model").val(),
+				year: $("#car-add-year").val(),
+				color: $("#car-add-color").val(),
+				description: $("#car-add-description").val()
+			};
 
-            var data = {
-                userEmail: email,
-                active: 1,
-                make: $("#addcar-make").val(),
-                model: $("#addcar-model").val(),
-                year: parseInt($("#addcar-year").val()),
-                color: $("#addcar-color").val(),
-                description: $("#addcar-description").val()
-            };
+			Driftr.api("POST", "Vehicle", data).done(function()
+			{
+				location.reload(true);
+			});
+		});
+	};
 
-            Driftr.api("POST", "Vehicle", data).done(function () {
-                window.location = "dashboard.html";
-            });
-        });
-    };
+	window.submitEdit = function()
+	{
+		Driftr.api("GET", "Login", null).done(function(user)
+		{
+			var data = {
+				userEmail: user.email,
+				active: $("#car-edit-active").is(":checked"),
+				make: $("#car-edit-make").val(),
+				model: $("#car-edit-model").val(),
+				year: $("#car-edit-year").val(),
+				color: $("#car-edit-color").val(),
+				description: $("#car-edit-description").val()
+			};
+
+			Driftr.api("PUT", "Vehicle/" + window.editId, data).done(function()
+			{
+				location.reload(true);
+			});
+		});
+	};
 });
-
-var populateVehicles = function (vehicles) {
-    //Create the base for the table
-    var table = document.createElement("table");
-    table.className = "table";
-
-    //build the headdings for the table
-    var thead = document.createElement("thead");
-    var headTr = document.createElement("tr");
-    var headYear = document.createElement("th");
-    var headVehicle = document.createElement("th");
-    var headMake = document.createElement("th");
-    var headColor = document.createElement("th");
-    var headDescription = document.createElement("th");
-    var headDelete = document.createElement("th");
-
-    //set text of each head
-    headYear.innerHTML = "Year";
-    headVehicle.innerHTML = "Model";
-    headMake.innerHTML = "Make";
-    headColor.innerHTML = "Color";
-    headDescription.innerHTML = "Description";
-    
-    headDelete.innerHTML = "";
-    //append headding stuff together
-    headTr.appendChild(headYear);
-    headTr.appendChild(headVehicle);
-    headTr.appendChild(headMake);
-    headTr.appendChild(headColor);
-    headTr.appendChild(headDescription);
-    headTr.appendChild(headDelete);
-    thead.appendChild(headTr);
-    //append thead into table
-    table.appendChild(thead);
-
-    //create tbody to add rows to
-    var tbody = document.createElement("tbody");
-
-    for (var i = 0; i < vehicles.length; i++) {
-        //build row and its columns
-        var bodyTr = document.createElement("tr");
-        var bodyYear = document.createElement("td");
-        var bodyVehicle = document.createElement("td");
-        var bodyMake = document.createElement("td");
-        var bodyColor = document.createElement("td");
-        var bodyDescription = document.createElement("td");
-        var bodyDelete = document.createElement("td");
-
-        //********SPECIAL FOR DELETE BUTTON*********
-        //create button
-        var buttonDelete = document.createElement("button");
-        buttonDelete.setAttribute("type", "button");
-        buttonDelete.className = "btn btn-danger";
-        buttonDelete.innerHTML = "DELETE"
-        buttonDelete.setAttribute("value", vehicles[i]['id']);
-        //set onClick action for delete button
-        buttonDelete.onclick = deleteFriend;
-        //******************************************
-
-        //populate the columns
-        bodyYear.innerHTML = vehicles[i]['year'];
-        bodyVehicle.innerHTML = vehicles[i]['model'];
-        bodyMake.innerHTML = vehicles[i]["make"];
-        bodyColor.innerHTML = vehicles[i]['color'];
-        bodyDescription.innerHTML = vehicles[i]['description'];
-        bodyDelete.appendChild(buttonDelete);
-
-        //append all columns into row
-        bodyTr.appendChild(bodyYear);
-        bodyTr.appendChild(bodyVehicle);
-        bodyTr.appendChild(bodyMake);
-        bodyTr.appendChild(bodyColor);
-        bodyTr.appendChild(bodyDescription);
-        bodyTr.appendChild(bodyDelete);
-
-        //append row into body of table
-        tbody.appendChild(bodyTr);
-    }
-    //append tbody into table
-    table.appendChild(tbody);
-    //append table into doc
-    $("#carlist").append(table);
-};
-
-var deleteFriend = function () {
-    var id = this.getAttribute("value");
-
-    Driftr.api("GET", "Login", null).done(function (curUser) {
-        var email = curUser.email;
- 
-		 Driftr.api("DELETE", "Vehicle?id="+id, null).done(function(){
-			 window.location = "vehicles.html";
-		 });
-		 
-    });
-};
